176c92896c181a8e590c45d0efd00989d6e4b31c
Akhil Kokani <akhilkokani@gmail.com>
2019-02-08 03:20:55 +0530

    Added homepage related files

---
+++ includes/header_info.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Contains header information which has to added in <head> tag
+ *
+ *
+ *
+ *
+ * @author Akhil Kokani
+ * @package SET
+ */
+?>
+
+<link rel="stylesheet" href="./styles/prix.css">
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Startup Ecosystem Tracker - CodeManiacs</title>
+
+  <?php include_once "./includes/header_info.php"; ?>
+  <link rel="stylesheet" href="./styles/prix.css">
+
+</head>
+<body>
+  
+</body>
+</html>
