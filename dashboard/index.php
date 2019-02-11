8df2d104702ad546b805a482b7890e2addc80633
Akhil Kokani <akhilkokani@gmail.com>
2019-02-11 19:36:32 +0530

    Added dashboard related files

---
+++ dashboard/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This page is just used to redirect user to specific page inside dashboard folder,
+ * such as /dashboard/startup/ etc...
+ *
+ *
+ *
+ * @author Akhil Kokani
+ * @package SET
+ */
+
+header ( "Location: ./startup/" );
+die();
---
+++ dashboard/startup/index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Startup Page Inside Dashboard
+ *
+ *
+ *
+ * @author Akhil Kokani
+ * @package SET
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>My Startup - Startup Ecosystem Tracker | CodeManiacs</title>
+
+  <link rel="icon" href="../../images/favicon.jpg">
+  <link rel="stylesheet" href="../../styles/prix.css">
+  <link rel="stylesheet" href="../../styles/index.css">
+  
+  <script src="../../scripts/jquery.js"></script>
+
+</head>
+<body>
+
+  <div class="startup-wrap">
+    <div class="dashboard-divider-wrap disp-flex">
+      <div class="">
+        
+      </div>
+    </div>
+  </div>
+  
+</body>
+</html>
