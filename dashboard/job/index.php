1bc7b3e18dea5e97f0315ea025abde7ed3541cd5
Akhil Kokani <akhilkokani@gmail.com>
2019-02-20 20:12:33 +0530

    Deisgned "Work Startup" Case in Job Section of Dashboard

--- dashboard/job/index.php
+++ dashboard/job/index.php
@@ -125,9 +125,74 @@ include_once "../_includes/check_login_status.php";
               </div>
             </div>
           </div>
+
+          <!-- Has been added as team member -->
+          <div class="dashboard-floor job-floor">
+            <div class="added-as-team-member-wrap">
+              <div class="added-as-team-member">
+                <div class="dboard-title-wrap disp-flex" style="margin:0;">
+                  <div class="work-icon-wrap" style="margin-right:5px;">
+                    <svg version="1.1" style="fill:#333333;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                      width="19px" height="19px" viewBox="0 0 512 512" xml:space="preserve">
+                      <g>
+                        <path d="M393.267,238.088l-0.231,10.404c-0.814,11.65-3.797,31.912-14.102,54.736c3.251-15.208,4.978-30.982,4.978-47.164
+                          c0-12.096-0.958-23.968-2.799-35.544c-15.091-94.901-89.626-169.923-184.138-185.518C185.706,32.285,179.048,32,179.048,32
+                          c0.238,0.281,0.465,0.562,0.7,0.844c39.458,47.181,44.1,96.656,37.744,131.85c-2.281,12.629-5.978,23.421-9.991,31.605
+                          c0,0,3.359-13.911,3.035-29.72c-0.293-14.234-3.572-30.006-14.986-38.552c3.497,18.378-0.752,33.501-9.121,47.729
+                          C161.69,217.808,101,249.386,96,327.408v3.836c0,53.692,25.624,98.979,68.719,125.012c-6.85-12.344-14.964-35.207-8.733-60.151
+                          c3.998,23.669,9.951,36.045,20.879,51.756c8.153,11.721,19.104,19.269,33.095,24.934S238.995,480,255.889,480
+                          c55.809,0,105.228-28.567,133.845-71.952l0.267,0.061v-0.007c16-25.254,26.1-55.5,26.1-88.019
+                          C416.1,290.112,407.596,262.071,393.267,238.088z"/>
+                      </g>
+                    </svg>
+                  </div>
+                  <div class="dboard-title">
+                    <span>Work</span>
+                  </div>
+                </div>
+
+                <div class="work-startup-wrap">
+                  <div class="work-startup disp-flex" style="height: 200px;">
+                    <div class="work-startup-logo-wrap">
+                      <div class="work-startup-logo vert-center">
+                        <img style="margin: auto;" src="../../images/default_startup_icon_dark.png" width="125" height="125" alt="">
+                      </div>
+                    </div>
+                    <div class="vert-center" style="margin: 0 5em 0 0;">
+                      <div class="work-startup-vertical-line"></div>
+                    </div>
+                    <div class="work-startup-details-wrap">
+                      <div class="work-startup-details vert-center">
+                        <div>
+                          <div class="work-startup-name-wrap">
+                            <div class="work-startup-name">
+                              <h2>Startup Name Goes Here</h2>
+                            </div>
+                          </div>
+                          <div class="work-startup-vision-wrap">
+                            <div class="work-startup-vision">
+                              <p>Startup Vision Goes here, it maybe long at max only one line.</p>
+                            </div>
+                          </div>
+                          <div class="work-startup-actions-wrap disp-flex" style="margin: 2em 0 0 0;">
+                            <div class="work-startup-visit-profile" style="font-size: 8px; margin: 0 2em 0 0;">
+                              <a href="../../view" target="_blank">View Profile</a>
+                            </div>
+                            <div class="work-startup-remove">
+                              <a style="font-size: 12px; font-weight: 100; color: #9e9e9e;" title="Clik to remove you from Startup Team">I don't work here</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           
           <!-- If user has not uploaded his/her CV -->
-          <div class="dashboard-floor investor-floor" style="display: none;">
+          <div class="dashboard-floor job-floor" style="display: none;">
             <div class="job-not-applied-wrap">
               <div class="job-not-applied">
                 <div class="job-not-applied-image-wrap">
